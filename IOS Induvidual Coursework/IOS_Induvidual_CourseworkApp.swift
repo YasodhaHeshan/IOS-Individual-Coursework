af19fd8ab413e4ff0233ef7500755c1e18572bbf
YasodhaHeshan <yasodhaheshan2002@gmail.com>
2026-04-10 13:30:43 +0530

    First Commit

---
+++ IOS Induvidual Coursework/IOS_Induvidual_CourseworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_Induvidual_CourseworkApp.swift
+//  IOS Induvidual Coursework
+//
+//  Created on 10/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct IOS_Induvidual_CourseworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
